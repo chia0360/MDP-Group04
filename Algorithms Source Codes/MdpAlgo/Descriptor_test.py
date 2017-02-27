7d2cc394e3ae3ef0f88f43b8da4db0cfdaf42750
TranscendentEK <joel_tan1505@hotmail.com>
2017-02-27 12:18:00 +0800

    decoupling of map and descriptor

--- Algorithms Source Codes/MdpAlgo/Descriptor_test.py
+++ Algorithms Source Codes/MdpAlgo/Descriptor_test.py
@@ -1,4 +1,6 @@
 import map
+import Arena
+import descriptor
 
 class test:
     def __init__(self):
@@ -38,14 +40,17 @@ class test:
 
 
         self.m = map.Map()
-        
+        self.des = descriptor.descriptor()
+        self.d = Arena.Arena()
 
     def exe(self):
-        self.m.map = self.map1
+        self.m.map = self.d.arena4
         print(self.m.map)
-        self.m.descriptor1()
-        self.m.map = self.map2
-        self.m.descriptor2()
+        self.des.map = self.m.map
+        self.des.descriptor1() 
+        print(self.des.descriptor1())
+        #self.m.map = self.map2
+        #self.m.descriptor2()
     def print_map(self):
         self.m.map = self.map1
        
